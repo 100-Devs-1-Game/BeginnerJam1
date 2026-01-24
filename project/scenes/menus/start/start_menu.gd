3ed40276c8d1c2f9edc5dc5ae004e79bb8cdb428
Kahn <255939722+kahnimoto@users.noreply.github.com>
2026-01-24 23:44:49 +0100

    cleaned up code in start menu

--- project/scenes/menus/start/start_menu.gd
+++ project/scenes/menus/start/start_menu.gd
@@ -10,12 +10,17 @@ var _start_location: Vector2i
 var _continue_location: Vector2i
 var _settings_location: Vector2i
 var _quit_location: Vector2i
+var _transition_time := 0.0
+var _transitioning := false
+var _from_camera: Camera2D
+var _to_camera: Camera2D
+var _exited_start_area_once := false
 
 @onready var start_game_area: Area2D = %StartGameArea
 @onready var continue_game_area: Area2D = %ContinueGameArea
 @onready var settings_area: Area2D = %SettingsArea
 @onready var quit_game_area: Area2D = %QuitGameArea
-@onready var shadow_hider: Area2D = %ShadowHider
+@onready var leaving_start_area: Area2D = %ShadowHider
 @onready var next_area_shadow: Sprite2D = %NextAreaShadow
 @onready var start_button: Button = %StartButton
 @onready var continue_button: Button = %ContinueButton
@@ -27,14 +32,13 @@ var _quit_location: Vector2i
 @onready var transition_camera: Camera2D = %TransitionCamera
 
 
-
 func _ready() -> void:
 	locked_menu_camera.make_current()
 	start_game_area.area_entered.connect(_on_player_entered_start_game)
 	continue_game_area.area_entered.connect(_on_player_entered_continue_game)
 	settings_area.area_entered.connect(_on_player_entered_settings)
 	quit_game_area.area_entered.connect(_on_player_entered_quit_game)
-	shadow_hider.area_entered.connect(_on_player_made_menu_choice)
+	leaving_start_area.area_entered.connect(_on_player_leaving_start_area)
 	next_area_shadow.show()
 	start_button.pressed.connect(_on_start_button_pressed)
 	start_button.mouse_entered.connect(_on_start_button_hovered)
@@ -51,11 +55,13 @@ func _ready() -> void:
 	_settings_location = tile_map.local_to_map($World/Settings/Path/Continue_1.global_position)
 	_quit_location = tile_map.local_to_map($World/Quit/Path/Quit_1.global_position)
 
+
 func _on_start_button_pressed() -> void:
 	if not _player_in_start_area:
 		return
 	if player.grid_position == _start_location:
 		player.force_move_player(Vector2.RIGHT, 1)
+
 
 func _on_start_button_hovered() -> void:
 	if not _player_in_start_area:
@@ -64,11 +70,13 @@ func _on_start_button_hovered() -> void:
 		return
 	player.force_move_player(Vector2.UP, 1)
 
+
 func _on_continue_button_pressed() -> void:
 	if not _player_in_start_area:
 		return
 	if player.grid_position == _continue_location:
 		player.force_move_player(Vector2.RIGHT, 1)
+
 
 func _on_continue_button_hovered() -> void:
 	if not _player_in_start_area:
@@ -77,11 +85,13 @@ func _on_continue_button_hovered() -> void:
 		return
 	pass
 
+
 func _on_settings_button_pressed() -> void:
 	if not _player_in_start_area:
 		return
 	if player.grid_position == _settings_location:
 		player.force_move_player(Vector2.RIGHT, 1)
+
 
 func _on_settings_button_hovered() -> void:
 	if not _player_in_start_area:
@@ -90,11 +100,13 @@ func _on_settings_button_hovered() -> void:
 		return
 	pass
 
+
 func _on_quit_button_pressed() -> void:
 	if not _player_in_start_area:
 		return
 	if player.grid_position == _quit_location:
 		player.force_move_player(Vector2.RIGHT, 1)
+
 
 func _on_quit_button_hovered() -> void:
 	if not _player_in_start_area:
@@ -105,12 +117,15 @@ func _on_quit_button_hovered() -> void:
 
 
 func _on_player_returned_to_start_area(_a) -> void:
-	_player_in_start_area = true
-	create_tween().tween_property(next_area_shadow, "modulate", Color(1, 1, 1, 1.0), 0.5)
-	_switch_camera(player.camera, locked_menu_camera)
+	if _exited_start_area_once:
+		_player_in_start_area = true
+		create_tween().tween_property(next_area_shadow, "modulate", Color(1, 1, 1, 1.0), 0.5)
+		_switch_camera(player.camera, locked_menu_camera)
+
 
 func _on_player_left_start_area(_a) -> void:
 	_player_in_start_area = false
+
 
 func _on_player_entered_start_game(_a) -> void:
 	get_tree().change_scene_to_packed.call_deferred(TEST_LEVEL)
@@ -129,10 +144,6 @@ func _on_player_entered_quit_game(_a) -> void:
 	print("quit_game")
 	get_tree().quit()
 
-var _transition_time := 0.0
-var _transitioning := false
-var _from_camera: Camera2D
-var _to_camera: Camera2D
 
 func _process(delta: float) -> void:
 	if _transitioning:
@@ -143,12 +154,14 @@ func _process(delta: float) -> void:
 			_to_camera.make_current()
 
 
-func _on_player_made_menu_choice(_a) -> void:
+func _on_player_leaving_start_area(_a) -> void:
 	if next_area_shadow.modulate == Color(0, 0, 0, 0):
 		return
+	_exited_start_area_once = true
 	create_tween().tween_property(next_area_shadow, "modulate", Color(0, 0, 0, 0), 0.5)
 	_switch_camera(locked_menu_camera, player.camera)
 	player.camera.enabled = true
+
 
 func _switch_camera(from: Camera2D, to: Camera2D) -> void:
 	_transitioning = true
