d38f9792f579945b08d674a70d9ad8477b24b1c2
Kahn <255939722+kahnimoto@users.noreply.github.com>
2026-01-24 12:29:02 +0100

    Implemented the start of the Player scene, includes only movement for now

---
+++ project/player/player.gd
@@ -0,0 +1,82 @@
+class_name Player
+extends Node2D
+
+#region signals
+signal moved_ended_at(location: Vector2i)
+#endregion
+
+#region constants
+const GRID_SIZE := 32  # TODO Move to a global defined constant
+const TURN_DURATION := 0.5  # TODO Move to a global defined constant
+#endregion
+
+#region class variables
+var grid_position := Vector2i.ZERO
+#endregion
+
+#region private variables
+var _is_moving := false
+var _last_requested_movement_direction := Vector2.ZERO
+#endregion
+
+#region onready variables
+@onready var visuals: Node2D = %Visuals
+@onready var shadow_sprite: Sprite2D = %ShadowSprite
+@onready var character_sprite: AnimatedSprite2D = %AnimatedCharacterSprite
+@onready var area: Area2D = %Area2D
+@onready var camera: Camera2D = %PlayerCamera2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+#endregion
+
+
+#region virtual methods
+func _ready() -> void:
+	grid_position = (global_position / GRID_SIZE) as Vector2i
+	area.area_entered.connect(_on_area_entered)
+
+
+func _process(_delta: float) -> void:
+	move_character()
+
+
+func move_character() -> void:
+	if _is_moving:
+		return
+	var desired_movement: Vector2
+	desired_movement = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if desired_movement.x != 0.0 and desired_movement.y != 0.0:
+		return  # Rejects input if tryint to move diagonally
+	if desired_movement == Vector2.ZERO:
+		return
+	if desired_movement.x:
+		character_sprite.flip_h = desired_movement.x < 0
+	_last_requested_movement_direction = desired_movement
+
+	# TODO Wall check
+	# TODO Initiate Turn
+	_is_moving = true
+	grid_position += desired_movement as Vector2i
+	# TODO Ice check and similar
+	var desired_global_pos: Vector2 = global_position + desired_movement * GRID_SIZE
+	character_sprite.play("walking")
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", desired_global_pos, TURN_DURATION).set_ease(Tween.EASE_IN)
+	tween.tween_callback(moved_ended_at.emit.bind(grid_position))
+	tween.tween_callback(character_sprite.play.bind("default"))
+	tween.tween_callback(func(): _is_moving = false)  # TODO do in response to turnmanager instead
+
+#endregion
+
+
+#region class handlers
+
+func _on_area_entered(other_area: Area2D) -> void:
+	print("Encountered other area: %s" % other_area)
+
+#endregion
+
+
+#region class methods
+
+
+#endregion
